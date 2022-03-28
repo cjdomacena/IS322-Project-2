b217ed2473ce236591c69b944ffd41e73ba7f328
cjdomacena <cjdomacena@yahoo.com>
2022-03-28 17:57:19 -0400

    style: :lipstick: change font

--- src/component/TodoCard/index.js
+++ src/component/TodoCard/index.js
@@ -81,7 +81,7 @@ function TodoCard({ todo, view })
 	return (
 		<div className={`w-full rounded-sm mx-auto p-4 bg-slate-800 text-neutral-100 border border-slate-700`}>
 			<div className="flex items-center justify-between space-x-2">
-				<h1 className="text-base font-serif capitalize">{todo.title}</h1>
+				<h1 className="text-lg font-medium capitalize">{todo.title}</h1>
 				<div className="flex">
 					<p className={`text-xs whitespace-nowrap bg-gray-900 px-2 rounded py-1 w-fit`}>{todo.type}</p>
 					<p className={`text-xs whitespace-nowrap ${ getColor() } px-2 rounded py-1`}>{todo.status}</p>
